586ad5c060f134e891a02f8ee0be35d8d5b62195
Romi Julianto <romyjulians@gmail.com>
2023-12-31 17:24:15 +0700

    feat: add badge and container product

--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -1,10 +1,27 @@
+import 'package:ecommerce_avatech/widget/HomeAppBar.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(children: [HomeAppBar(),],)
+        body: ListView(
+      children: [
+        const HomeAppBar(),
+        Container(
+          /* TODO: temporary height */
+          height: 500,
+          padding: const EdgeInsets.only(),
+          decoration: const BoxDecoration(
+            color: Color(0XFFEDECF2),
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(35),
+              topRight: Radius.circular(35),
+            ),
+          ),
+        ),
+      ],
+    )
     );
   }
 }
--- lib/widget/HomeAppBar.dart
+++ lib/widget/HomeAppBar.dart
@@ -1,1 +1,49 @@
-import "" 
+import 'package:badges/badges.dart' as badges;
+import "package:flutter/material.dart";
+
+class HomeAppBar extends StatelessWidget {
+  const HomeAppBar({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      padding: const EdgeInsets.all(25),
+      child: Row(children: [
+        const Icon(
+          Icons.sort,
+          size: 30,
+          color: Color(0xFF4C53A5),
+        ),
+        const Padding(
+          padding: EdgeInsets.only(
+            left: 20,
+          ),
+          child: Text(
+            "Avatech Ecommerce",
+            style: TextStyle(
+                fontSize: 23,
+                fontWeight: FontWeight.bold,
+                color: Color(0xFF4C53A5)),
+          ),
+        ),
+        const Spacer(),
+        badges.Badge(
+          showBadge: true,
+          ignorePointer: false,
+          badgeContent: const Text(
+            '3',
+            style: TextStyle(color: Colors.white),
+          ),
+          child: InkWell(
+            onTap: () {},
+            child: const Icon(
+              Icons.shopping_bag_outlined,
+              size: 32,
+              color: Color(0xFF4C53A5),
+            ),
+          ),
+        ),
+      ]),
+    );
+  }
+}
